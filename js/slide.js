71152d37230d9d29f1a310a312bf29a4fb4c38f0
Lucas Silva <lucas.silva182@outlook.com>
2022-12-08 19:13:17 -0300

    nav prev e next

--- js/slide.js
+++ js/slide.js
@@ -10,6 +10,10 @@ export default class Slide {
         }
     }
 
+    transition(active){
+        this.slide.style.transition = active ? 'transform .3s' : '';
+    }
+
     moveSlide(distX){
         this.dist.movePosition = distX
         this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
@@ -31,6 +35,7 @@ export default class Slide {
             movetype = 'touchmove';
         }
         this.wrapper.addEventListener(movetype, this.onMove);
+        this.transition(false);
     }
 
     onMove(event){
@@ -43,6 +48,18 @@ export default class Slide {
         const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
         this.wrapper.removeEventListener(moveType, this.onMove);
         this.dist.finalPosition = this.dist.movePosition;
+        this.transition(true);
+        this.changeSLideOnEnd();
+    }
+
+    changeSLideOnEnd(){
+        if (this.dist.movement > 120 && this.index.next !== undefined) {
+            this.activeNextSlide();
+        } else if(this.dist.movement < 120 && this.index.next !== undefined) {
+            this.activePrevSlide()
+        } else {
+            this.changeSlide(this.index.active);
+        }
     }
 
     addSlideEvents(){
@@ -88,11 +105,21 @@ export default class Slide {
     changeSlide(index){
         const activeSlide = this.slideArray[index];
         this.moveSlide(activeSlide.position);
+        this.slideIndexNav(index);
         this.dist.finalPosition = activeSlide.position;
+    }
+
+    activePrevSlide(){
+        if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+    }
+
+    activeNextSlide(){
+        if (this.index.next !== undefined) this.changeSlide(this.index.next);
     }
 
     init(){
         this.bindEvents();
+        this.transition(true);
         this.addSlideEvents();
         this.slidesConfig();
         return this
